50b0a4438c4ab147a2a8c35f7d72729a69aecad1
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-04-21 16:33:04 +0900

    Update le-db-connect.php

--- craft-admin/le-db-connect.php
+++ craft-admin/le-db-connect.php
@@ -1,4 +1,21 @@
 <?php
-$db = mysqli_connect('localhost','root','root','craft_main') or die(mysqli_connect_error());
-mysqli_set_charset($db,'utf8');
+try{
+	$dbh = new PDO(
+		'mysql:host=localhost; dbname=craft_main; charset=utf8',
+		'root',
+		'root',
+		array(
+			//throw error mode ON
+			PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION,
+			//Emulater OFF
+			PDO::ATTR_EMULATE_PREPARES => false,
+		)
+	); 
+
+}catch(PDOException $e){
+	$error = $e->getMessage();
+}
+//mysqli ver
+//$db = mysqli_connect('localhost','root','root','craft_main') or die(mysqli_connect_error());
+//mysqli_set_charset($db,'utf8');
 ?>
